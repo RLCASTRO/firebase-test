e9acdae4ee0fcab9408488a00a594b6561cc64fd
Rodrigo Castro <castro.rl@gmail.com>
2023-05-25 14:58:05 -0300

    Created 3 pages to play with. Installed and coded the react-router-dom routes, installed the firebase, initialized it on the index.js, and stored all the configurations into a config file. The signin and signup are working good, returning the response to the console so far.

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyATWmE6czrcFZmTtX7vem9NEUZM1EZqp2s",
+  authDomain: "frontboilerplate-dev.firebaseapp.com",
+  projectId: "frontboilerplate-dev",
+  storageBucket: "frontboilerplate-dev.appspot.com",
+  messagingSenderId: "626805481376",
+  appId: "1:626805481376:web:7a11347b13c88011c18ec5",
+  measurementId: "G-JSP9E91QX7"
+};
