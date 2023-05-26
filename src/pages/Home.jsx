efdac244477efc75816a4b66bf6d96b839f1e672
Rodrigo Castro <castro.rl@gmail.com>
2023-05-26 15:39:31 -0300

    updating the gh-pages config

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,11 @@ import { Navigate } from "react-router-dom";
 
 const Home = ({ isLoggedIn }) => {
   console.log('Home', isLoggedIn);
-  return isLoggedIn ? <div>Home</div> : <Navigate to="/" />;
+  return isLoggedIn 
+  ? 
+    <div>Home</div> 
+  : 
+  <Navigate to="/" />;
 };
 
 export default Home;
